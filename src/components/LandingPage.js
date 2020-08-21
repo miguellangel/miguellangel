d87ddf7a08c302205ac5875ea0728636d9e0a299
Miguel <arriaga.miguel123@gmail.com>
2020-08-20 19:26:04 -0500

    remove gitignore files

---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <>
+            <div>
+                <h1>My name is Miguel Arriaga</h1>
+            </div>
+        </>
+    )
+}
+
+export default LandingPage;
