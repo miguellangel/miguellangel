6851f634fa18fb3f80ec65ef6af8facbe4dd2562
Miguel <arriaga.miguel123@gmail.com>
2020-09-30 18:25:26 -0500

    Updates

---
+++ precache-manifest.97eecb341b644c2b8f39e4e72cf35d78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b873be19dea6de10c62416db786a5a",
+    "url": "/miguellangel.github.io/index.html"
+  },
+  {
+    "revision": "8534dbc21a2a89325d02",
+    "url": "/miguellangel.github.io/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "006d67d035b62402aa2b",
+    "url": "/miguellangel.github.io/static/css/main.ab8b21d3.chunk.css"
+  },
+  {
+    "revision": "8534dbc21a2a89325d02",
+    "url": "/miguellangel.github.io/static/js/2.0c6ca3ee.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/miguellangel.github.io/static/js/2.0c6ca3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006d67d035b62402aa2b",
+    "url": "/miguellangel.github.io/static/js/main.54f8904b.chunk.js"
+  },
+  {
+    "revision": "9c02d6d0a0157b722568",
+    "url": "/miguellangel.github.io/static/js/runtime-main.8495142d.js"
+  }
+]);
