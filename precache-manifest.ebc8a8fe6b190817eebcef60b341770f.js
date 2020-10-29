3ad4c6c5f02272a0a9345420aac49d318e9ba6f0
Miguel <arriaga.miguel123@gmail.com>
2020-10-29 18:39:47 -0500

    Updates

--- precache-manifest.ebc8a8fe6b190817eebcef60b341770f.js
+++ precache-manifest.ebc8a8fe6b190817eebcef60b341770f.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5b9f9c0dc6ec49eb7fdb7aea3b52aa6e",
+    "revision": "fb0c791f5c7ed423c2477cbd11352014",
     "url": "/miguellangel.github.io/index.html"
   },
   {
-    "revision": "ab2d4b2b5bc56fb9d340",
+    "revision": "5320ae0d2f8dde75b3fc",
     "url": "/miguellangel.github.io/static/css/2.11829350.chunk.css"
   },
   {
-    "revision": "f1ff1dca56b816f5e6d9",
-    "url": "/miguellangel.github.io/static/css/main.9ec63354.chunk.css"
+    "revision": "41f185b31b2874cb4d08",
+    "url": "/miguellangel.github.io/static/css/main.381ef8b5.chunk.css"
   },
   {
-    "revision": "ab2d4b2b5bc56fb9d340",
-    "url": "/miguellangel.github.io/static/js/2.ff123b40.chunk.js"
+    "revision": "5320ae0d2f8dde75b3fc",
+    "url": "/miguellangel.github.io/static/js/2.15f53819.chunk.js"
   },
   {
-    "revision": "4aed591473bc421c94818a144abace83",
-    "url": "/miguellangel.github.io/static/js/2.ff123b40.chunk.js.LICENSE.txt"
+    "revision": "5f683975dc1705403d2e982b6b869a5a",
+    "url": "/miguellangel.github.io/static/js/2.15f53819.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f1ff1dca56b816f5e6d9",
-    "url": "/miguellangel.github.io/static/js/main.976ca0ac.chunk.js"
+    "revision": "41f185b31b2874cb4d08",
+    "url": "/miguellangel.github.io/static/js/main.e9431b45.chunk.js"
   },
   {
     "revision": "9c02d6d0a0157b722568",
