875c205c0012034ae00a1b92c2b281e9e6e8e2ba
Miguel <arriaga.miguel123@gmail.com>
2021-01-03 13:50:28 -0600

    add different svg viewBox parameters per breakpoint

--- src/App.js
+++ src/App.js
@@ -12,16 +12,8 @@ const App = ({breakpoints, currentBreakpoint}) => {
 	
 	
 	React.useEffect(() => {
-		if (breakpoints[currentBreakpoint] < breakpoints.mobileLandscape) {
-			document.getElementsByTagName('html')[0].setAttribute('isMobile', true)
-			// document.getElementById('navCurveSVG').setAttribute('viewBox', '0 0 300 65')
-			// document.getElementById('navCurveSVG').setAttribute('')
-			// Array.from(document.getElementsByClassName('sectionCurveSVG')).map((sectionSVG) => sectionSVG.setAttribute('viewBox', '0 0 300 32'))
-		} else {
-			document.getElementsByTagName('html')[0].setAttribute('isMobile', false)
-			// document.getElementById('navCurveSVG').setAttribute('viewBox', '0 0 600 32')
-			// Array.from(document.getElementsByClassName('sectionCurveSVG')).map((sectionSVG) => sectionSVG.setAttribute('viewBox', '0 0 600 32'))
-		}
+		breakpoints[currentBreakpoint] < breakpoints.mobileLandscape ? document.getElementsByTagName('html')[0].setAttribute('isMobile', true)
+		: document.getElementsByTagName('html')[0].setAttribute('isMobile', false)
 	})
 
 	return (
