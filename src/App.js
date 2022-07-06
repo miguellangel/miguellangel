745500075aeff2711dbbeea79551a6e13a9343cd
miguellangel <arriaga.miguel123@gmail.com>
2022-07-06 15:57:31 -0500

    add button polyfill for firefox mobile touch/scroll events

--- src/App.js
+++ src/App.js
@@ -8,10 +8,36 @@ import Projects from './components/Projects'
 
 const App = () => {
 
+	const isFirefox = React.useRef(navigator.userAgent.toLowerCase().indexOf('firefox') > -1)
+	const t = React.useRef()
+	const handleScroll = (direction) => (e) => {
+		e.preventDefault()
+		let target = e.target.parentNode.parentNode
+		let wheelEvent = new WheelEvent('wheel', {
+			deltaY: direction === 'up' ? -1 : 1,
+			deltaMode: 1
+		});
+		
+		target.dispatchEvent(wheelEvent)
+		
+
+		//direction === 'up' ? target.scrollBy(0, 50) : target.scrollBy(0, -50)
+
+	}
+
+
+	React.useEffect(() => {
+		console.log(isFirefox)
+	})
+
 	return (
 		<>
 			<Sidebar />	
 			<div className="content container-true">
+				<div id="firefox-helper">
+					<button id="_up" onClick={handleScroll('up')}>↑</button>
+					<button id="_down" onClick={handleScroll('down')}>↓</button>
+				</div>
 				<About />
 				<Skills />
 				<Projects />
